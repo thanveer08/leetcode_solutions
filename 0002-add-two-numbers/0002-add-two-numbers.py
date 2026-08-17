c4c3964338e4d572a05bdbd8edfc91bb9fb07a8b
Thanveer <thanveer08@proton.me>
2026-08-17 12:20:48 +0530

    Time: 3 ms (64.89%), Space: 19.3 MB (41.02%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,27 +3,29 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        arr1 = []
-        temp = l1
-        while temp:
-            arr1.append(temp.val)
-            temp = temp.next
-
-        arr2 = []
-        temp = l2
-        while temp:
-            arr2.append(temp.val)
-            temp = temp.next
-
-        result = int("".join(str(n) for n in reversed(arr1))) + int("".join(str(n) for n in reversed(arr2)))
-
+        t1 = l1
+        t2 = l2
         dummy = ListNode(0)
         curr = dummy
-        for digit in reversed(str(result)):
-            curr.next = ListNode(int(digit))
+        carry = 0
+        while t1 or t2:
+            sums = carry
+            if t1:
+                sums = sums+ t1.val
+            if t2:
+                sums = sums + t2.val
+            new_node = ListNode(sums%10)
+            carry = sums//10
+            curr.next = new_node
             curr = curr.next
-
+            if t1: 
+                t1 = t1.next
+            if t2:
+                t2 = t2.next
+        if carry:
+            new_node = ListNode(carry)
+            curr.next = new_node
+            curr = curr.next                    
         return dummy.next
